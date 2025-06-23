45b5ef2aa22a94bdcd22ec967f42bbcd4274a16c
fmleem3 <fmleem3@gmail.com>
2025-06-23 18:02:39 +0900

    array

---
+++ java/pjt/pjt20250623/src/com/office/ex/ArrayEx.java
@@ -0,0 +1,28 @@
+package com.office.ex;
+
+public class ArrayEx {
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5}; //길이5 고정됨
+		int nums1[] = {10,20,30}; //길이3 고정됨
+		int[] nums2 = new int[5];
+		nums2[0] = 1;
+		nums2[1] = 2;
+		nums2[2] = 3;
+		nums2[3] = 4;
+		nums2[4] = 5;
+		int[] nums3 = new int[] {1,2,3,4,5};
+		num3[num3.length = 1]
+				초기화안하면 0
+				// foreach
+				for(int num:num3) { System.out.println("num:"+num);}
+		int[] studentsScores = {80,95,100,70,68,74,88,99\8, 100,76};
+		int total = 0;
+		float average = 0.0f;
+		for(int score: studentsScores) {
+			total += score;
+		}
+		
+		
+	}
+
+}
---
+++ java/pjt/pjt20250623/src/com/office/ex/Loop.java
@@ -0,0 +1,21 @@
+package com.office.ex;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.for 횟수에 의한 반복
+		Scanner scnner = new Scanner( System.in);
+		System.print("반복횟수 입력:");
+		int loopCnt = scanner.nextint();
+		
+		for( int i=0; i< loopCnt; i++) {
+			System.out.println("Hello");
+		}
+		scanner.close();
+		
+		//2.while
+
+	}
+
+}
